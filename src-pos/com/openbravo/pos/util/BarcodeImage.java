836498893e920d3e30b390080d6f04157551c0de
Adrián Romero <adrian.romero@openbravo.com>
2008-04-30 07:21:42 +0000

    Added: New product labels report
    --HG--
    extra : convert_revision : svn%3A237dfc2b-7c0e-0410-bfc4-b1bdb0875535/trunk%40143

--- src-pos/com/openbravo/pos/util/BarcodeImage.java
+++ src-pos/com/openbravo/pos/util/BarcodeImage.java
@@ -15,6 +15,7 @@ import org.krysalis.barcode4j.BarcodeDimension;
 import org.krysalis.barcode4j.HumanReadablePlacement;
 import org.krysalis.barcode4j.impl.AbstractBarcodeBean;
 import org.krysalis.barcode4j.impl.code128.Code128Bean;
+import org.krysalis.barcode4j.impl.upcean.EAN13Bean;
 import org.krysalis.barcode4j.output.java2d.Java2DCanvasProvider;
 
 /**
@@ -23,17 +24,27 @@ import org.krysalis.barcode4j.output.java2d.Java2DCanvasProvider;
  */
 public class BarcodeImage {
     
+    public static Image getBarcodeEAN13(String value) {
+        AbstractBarcodeBean barcode = new EAN13Bean();
+        barcode.setMsgPosition(HumanReadablePlacement.HRP_BOTTOM);
+        return getBarcode(value, barcode);
+    }
+    
     public static Image getBarcode128(String value) {
-        
         AbstractBarcodeBean barcode = new Code128Bean();
+        barcode.setMsgPosition(HumanReadablePlacement.HRP_NONE);
+        return getBarcode(value, barcode); 
+    }
+    
+    private static Image getBarcode(String value, AbstractBarcodeBean barcode) {
+        
         barcode.setModuleWidth(1.0); 
         barcode.setBarHeight(40.0);
         barcode.setFontSize(10.0);
         barcode.setQuietZone(10.0);
         barcode.doQuietZone(true);                
-        barcode.setMsgPosition(HumanReadablePlacement.HRP_NONE);
         BarcodeDimension dim = barcode.calcDimensions(value);
-        int width = (int) dim.getWidth(0);
+        int width = (int) dim.getWidth(0) + 20;
         int height = (int) dim.getHeight(0);        
         
         BufferedImage imgtext = new BufferedImage(width, height,  BufferedImage.TYPE_INT_RGB);
@@ -47,14 +58,13 @@ public class BarcodeImage {
         try {
             barcode.generateBarcode(new Java2DCanvasProvider(g2d, 0), value);
         } catch (IllegalArgumentException e) {
-            g2d.drawRect(0, 0, width, height);
-            g2d.drawLine(0, 0, width, height);
-            g2d.drawLine(width, 0, 0, height);
+            g2d.drawRect(0, 0, width - 1, height - 1);
+            // g2d.drawLine(0, 0, width, height);
+            // g2d.drawLine(width, 0, 0, height);
         }
         
         g2d.dispose();
         
         return imgtext;  
     }
-
 }
