7e05c15c68bbac3048b53017f004828892832898
Adrián Romero <adrian.romero@openbravo.com>
2008-06-10 10:48:14 +0000

    Fixed. Start scripts for Windows
    --HG--
    extra : convert_revision : svn%3A237dfc2b-7c0e-0410-bfc4-b1bdb0875535/trunk%40190

--- configure.bat
+++ configure.bat
@@ -18,8 +18,11 @@ REM    You should have received a copy of the GNU General Public License
 REM    along with this program; if not, write to the Free Software
 REM    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-set CP="%CD%/openbravopos.jar"
 
-set CP=%CP%;"%CD%/locales/"
+set DIRNAME=%~dp0
+
+set CP="%DIRNAME%openbravopos.jar"
+
+set CP=%CP%;"%DIRNAME%locales/"
 
 start /B javaw -cp %CP% com.openbravo.pos.config.JFrmConfig
