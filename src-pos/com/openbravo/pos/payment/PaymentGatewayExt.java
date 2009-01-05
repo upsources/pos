b664b95796f8859ae51b04a4222204fb6859a182
Mikel Irurita <mikel.irurita@openbravo.com>
2009-01-05 09:04:58 +0000

    Fixes issue 0005358: Close sale with External payment gateway
    --HG--
    extra : convert_revision : svn%3A237dfc2b-7c0e-0410-bfc4-b1bdb0875535/trunk%40408

--- src-pos/com/openbravo/pos/payment/PaymentGatewayExt.java
+++ src-pos/com/openbravo/pos/payment/PaymentGatewayExt.java
@@ -1,5 +1,5 @@
 //    Openbravo POS is a point of sales application designed for touch screens.
-//    Copyright (C) 2007 Openbravo, S.L.
+//    Copyright (C) 2008 Openbravo, S.L.
 //    http://sourceforge.net/projects/openbravopos
 //
 //    This program is free software; you can redistribute it and/or modify
@@ -14,7 +14,7 @@
 //
 //    You should have received a copy of the GNU General Public License
 //    along with this program; if not, write to the Free Software
-//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//    Foundation, Inc., 51 Franklin Street, Fifth floor, Boston, MA  02110-1301  USA
 
 package com.openbravo.pos.payment;
 
@@ -25,6 +25,6 @@ public class PaymentGatewayExt implements PaymentGateway {
     }
   
     public void execute(PaymentInfoMagcard payinfo) {
-        //payinfo.paymentOK("OK"); // Este es el codigo de actualizacion...
+        payinfo.paymentOK("OK", payinfo.getTransactionID() , "");
     }
 }
