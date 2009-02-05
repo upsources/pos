400eaf2f14f1d766ce2fa4f4179ac82fc1d78de8
Jaroslaw Wozniak <jwozniak@openbravo.com>
2009-02-05 10:57:00 +0000

    Issue 5361: added Jetty and modified build.xml
    --HG--
    extra : convert_revision : svn%3A237dfc2b-7c0e-0410-bfc4-b1bdb0875535/trunk%40451

---
+++ jetty/pdastart.bat
@@ -0,0 +1,21 @@
+@echo off
+
+REM   Openbravo POS is a point of sales application designed for touch screens.
+REM   Copyright (C) 2007-2009 Openbravo, S.L.
+REM   http://sourceforge.net/projects/openbravopos
+REM
+REM   This program is free software; you can redistribute it and/or modify
+REM   it under the terms of the GNU General Public License as published by
+REM   the Free Software Foundation; either version 2 of the License, or
+REM   (at your option) any later version.
+REM
+REM   This program is distributed in the hope that it will be useful,
+REM   but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM   GNU General Public License for more details.
+REM
+REM   You should have received a copy of the GNU General Public License
+REM   along with this program; if not, write to the Free Software
+REM   Foundation, Inc., 51 Franklin Street, Fifth floor, Boston, MA  02110-1301  USA  
+
+java -jar start.jar
