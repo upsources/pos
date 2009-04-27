e1a9af4878a514a1f8c0b03e4484189bb68e1061
Adrián Romero <adrian.romero@openbravo.com>
2009-04-27 21:51:31 +0000

    Fixes issue 8789: Several javacript functions are not used
    --HG--
    extra : convert_revision : svn%3A237dfc2b-7c0e-0410-bfc4-b1bdb0875535/trunk%40602

--- web/tableScript.js
+++ web/tableScript.js
@@ -1,4 +1,3 @@
-
 //    Openbravo POS is a point of sales application designed for touch screens.
 //    Copyright (C) 2007-2009 Openbravo, S.L.
 //    http://sourceforge.net/projects/openbravopos
@@ -18,105 +17,12 @@
 //    You should have received a copy of the GNU General Public License
 //    along with Openbravo POS.  If not, see http://www.gnu.org/licenses/.
 
-var categoryId;
 var floorId;
-
-function getIndexBack(place){
-
-    var tables = document.getElementsByTagName('table');
-    var score = 'place='+place+ '&cat=' + categoryId + '&';
-	for (var m=0; m<tables.length; m++) {
-		if (tables[m].className == 'pickme') {
-				var tbodies = tables[m].getElementsByTagName('tbody');
-				for (var j=0; j<tbodies.length; j++) {
-					var rows = tbodies[j].getElementsByTagName('tr');
-                    for (var i=0; i<rows.length; i++) {
-                        if(rows[i].className == 'highlight selected' || rows[i].className == 'odd highlight selected'){
-                            score += 'parameters=' + i +'&';
-                        }
-                    }
-
-                }
-        }
-    }
-    window.location = 'addProduct.do?' + score;
-}
-
-function getIndexBackByRemoving(place){
-
-    var tables = document.getElementsByTagName('table');
-    var score = 'id=' + place +'&mode=1&';
-	for (var m=0; m<tables.length; m++) {
-		if (tables[m].className == 'pickme') {
-				var tbodies = tables[m].getElementsByTagName('tbody');
-				for (var j=0; j<tbodies.length; j++) {
-					var rows = tbodies[j].getElementsByTagName('tr');
-                    for (var i=0; i<rows.length; i++) {
-                        if(rows[i].className == 'highlight selected' || rows[i].className == 'odd highlight selected'){
-                            score += 'parameters=' + i +'&';
-                        }
-                    }
-
-                }
-        }
-    }
-    window.location='showPlace.do?' + score;
-}
-
-function getIndexBackByEditing(place){
-
-    var tables = document.getElementsByTagName('table');
-    var score = 'id=' + place +'&mode=0&';
-	for (var m=0; m<tables.length; m++) {
-		if (tables[m].className == 'pickme') {
-				var tbodies = tables[m].getElementsByTagName('tbody');
-				for (var j=0; j<tbodies.length; j++) {
-					var rows = tbodies[j].getElementsByTagName('tr');
-                    for (var i=0; i<rows.length; i++) {
-                        if(rows[i].className == 'highlight selected' || rows[i].className == 'odd highlight selected'){
-                            score += 'parameters=' + i +'&';
-                        }
-                    }
-
-                }
-        }
-    }
-
-    window.location ='modifyProduct.do?' + score;
-}
 
 function getIndexBackByEditing(nr, place){
     var score = 'id=' + place +'&mode=2&parameters=' + nr + '&parameters=' + document.getElementById('input'+nr).value;
 
     window.location ='showPlace.do?' + score;
-}
-
-function getIndexBackByAdding(nr, place){
-
-    var score = 'id=' + place +'&mode=3&parameters=' + nr;
-
-    window.location ='showPlace.do?' + score;
-}
-
-/*
- * Windows Mobile 5.0 Only
- */
-function getIndexBackByAddingWM5(nr, place){
-
-   var score = 'place=' + place + '&cat=' + categoryId +  '&parameters=' + nr;
-
-    window.location ='addProduct.do?' + score;
-}
-
-function getIndexBackByRemoving(nr, place){
-
-    var score = 'id=' + place +'&mode=1&parameters=' + nr;
-
-    window.location='showPlace.do?' + score;
-}
-
-function setCategoryId(id){
-    categoryId = id;
 }
 
 function saveFloorId(id){
@@ -126,8 +32,3 @@ function saveFloorId(id){
 function getLocation(place){
    window.location = 'showPlace.do?id='+ place + '&floorId=' +floorId;
 }
-
-//addLoadEvent(stripeTables);
-
-
-
