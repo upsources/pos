f604fa8ace4b80386aba5482a8ffcb0de662f216
Adrián Romero <adrianromero@openbravo.com>
2010-09-16 17:21:12 +0200

    Fixes issue 0014586: AMEX Cards still do not work in Openbravo POS It has been fixed the Luhn algorithm that did not work for odd card number lenghts
    --HG--
    extra : transplant_source : KL%60%26%CA%BF%80-%96%94%3B%F7%7F%DA%1D%23J%8F%8B%93

--- src-pos/com/openbravo/pos/util/LuhnAlgorithm.java
+++ src-pos/com/openbravo/pos/util/LuhnAlgorithm.java
@@ -31,14 +31,17 @@ public class LuhnAlgorithm {
     
     public static boolean checkCC(String cardNumber){
         int sum = 0;
-        
+        int flip = 0;
+
         if ( !StringUtils.isNumber(cardNumber) ){
             return false;
         }
-        
-        for (int i = 0; i < cardNumber.length(); i++) {
-            int k = Integer.parseInt(cardNumber.substring(i, i+1));
-            if ( i % 2 == 0 ) {
+
+        for (int i = cardNumber.length() -1; i >= 0; i--) {
+            int k = Character.digit(cardNumber.charAt(i), 10);
+            flip ++;
+
+            if ( flip % 2 == 0 ) {
                 k *= 2;
                 if (k > 9) {
                     k -= 9;
@@ -48,5 +51,18 @@ public class LuhnAlgorithm {
         }
         return (sum % 10 == 0);
     }
-    
+
+
+//    public static void main(String[] args) {
+//
+//        // Testing sample numbers
+//
+//        System.out.println(LuhnAlgorithm.checkCC("4111111111111111")); // Visa
+//        System.out.println(LuhnAlgorithm.checkCC("5500000000000004")); // Master card
+//        System.out.println(LuhnAlgorithm.checkCC("340000000000009")); // AMEX
+//        System.out.println(LuhnAlgorithm.checkCC("30000000000004")); // Dinners
+//        System.out.println(LuhnAlgorithm.checkCC("30000000000004")); // Carte blanche
+//        System.out.println(LuhnAlgorithm.checkCC("6011000000000004")); // Discover
+//        System.out.println(LuhnAlgorithm.checkCC("201400000000009")); // EnRoute
+//    }
 }
